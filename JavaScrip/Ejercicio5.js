d8e5f557e00aab63a49dc04b8848bf4f1f7997a2
AndresLandinez <andres_landinez@hotmail.com>
2023-08-14 20:40:03 -0500

    Boton,CSS y ejercicio 5

---
+++ JavaScrip/Ejercicio5.js
@@ -0,0 +1,10 @@
+/**Ejercicio 5: Mensajes personalizados
+Crea una función llamada mostrarMensaje que tome un parámetro nombre. 
+Dentro de la función, utiliza alert() para mostrar: "¡Hola, [nombre]! 
+Espero que estés disfrutando de JavaScript." Pide al usuario su nombre utilizando prompt() y llama a la función.
+(onclick) */
+
+function showsms() {
+    const name = prompt("Ingrese su nombre:")
+    alert("Hola " + name + "\nEspero que estes disfrutando de JavaScrip")
+}
